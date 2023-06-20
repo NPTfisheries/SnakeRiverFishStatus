b3f4ac54005199ff23a6f01bc0cd3cb96230354e
Mike Ackerman <dirtpathwanderer@gmail.com>
2023-06-20 14:26:52 -0600

    draft of 00_save_LGTrappingDBase.R

---
+++ scripts/00_save_LGTrappingDBase.R
@@ -0,0 +1,49 @@
+# -----------------------
+# Author(s): Ryan Kinzer and Mike Ackerman
+# Purpose: Open connection to the Lower Granite Dam trapping database (LGTrappingDB)
+#   and save table as a .csv
+# 
+# Created Date: Unknown
+#   Last Modified: June 20, 2023
+#
+# Notes:
+
+# clear environment
+rm(list = ls())
+
+# load packages
+library(tidyverse)
+library(here)
+
+# Requires a copy of the Lower Granite Dam trapping database and the ODBC driver
+if(.Platform$OS.type != "unix") { # includes windows machines
+  
+  # source LGTrappingDB function
+  source(here("R/loadLGTrappingDBase.R"))
+  
+  # set path to LGTrappingDB Access database
+  trap_filepath = here("data/LGTrappingDB/LGTrappingExportJodyW.accdb")
+  
+  # connect to LGTrappingDB
+  con = loadLGTrappingDBase(trapDB_filepath = trap_filepath)
+  
+  # read master table within LGTrappingDB
+  trap_dbase = DBI::dbReadTable(con, 'tblLGDMasterCombineExportJodyW')
+  
+  # and disconnect
+  DBI::dbDisconnect(con)
+  
+} # end if not unix
+
+# write .csv of LGTrappingDB for later use
+write_csv(trap_dbase, file = paste0(here("data/LGTrappingDB/LGTrappingDB_/"), Sys.Date(), ".csv"))
+
+# END SCRIPT
+
+# this works for Mac, or any system really
+# if(.Platform$OS.type == "unix") {
+#   trap_filepath <- './data/TrappingDBase/LGTrappingExportJodyW.accdb'
+#   trap_filepath = here("/data/TrappingDBase/tblLGDMasterCombineExportJodyW.csv")
+#   trap_dbase = readLGRtrapDB(trap_filepath)
+# }
+
