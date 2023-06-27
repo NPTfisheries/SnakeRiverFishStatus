375b5dc9ad7030b219d9b350005e1294ef5b2325
Mike Ackerman <dirtpathwanderer@gmail.com>
2023-06-27 09:15:12 -0600

    created draft of 03_create_valid_tag_lists.R

---
+++ scripts/03_create_valid_tag_lists.R
@@ -0,0 +1,56 @@
+# -----------------------
+# Author(s): Kevin See, Ryan Kinzer, and Mike Ackerman
+# Purpose: Create valid tag lists for LGR
+# 
+# Created Date: May 1, 2019
+#   Last Modified: June 27, 2023
+#
+# Notes: 
+
+# clear environment
+rm(list = ls())
+
+# load necessary libraries
+library(tidyverse)
+library(here)
+library(lubridate)
+
+# set up folder structure
+tags_folder = here("data/valid_tag_lists")
+if(!dir.exists(tags_folder)) {
+  dir.create(tags_folder)
+}
+
+# read csv of LGTrappingDB
+trap_df = read_csv(here("data/LGTrappingDB/LGTrappingDB_2023-06-27.csv"))
+
+# set species and spawn year
+spc = "Chinook"
+yr  = 2010
+
+if(spc == "Chinook")   { spc_code = 1 }
+if(spc == "Steelhead") { spc_code = 3 }
+
+# filter to valid tag list
+valid_df = trap_df %>%
+  filter(grepl(paste0('^', spc_code), SRR)) %>% # keep only the desired species
+  filter(SpawnYear == paste0("SY", yr)) %>%     # keep only the desired spawn year
+  filter(LGDLifeStage == "RF") %>%              # keep only adults (returning fish)
+  # From Baum et al. 2022: Trapped fish that were missing data for any of the following fields were considered invalid: date
+  # of collection, species, FL, origin (hatchery or wild), or adipose fish status (ad-clipped or ad-intact). Trapped fish less
+  # than 30 cm (FL) were considered invalid as they are not identified to species at the USACE fish-counting window
+  filter(LGDValid == 1) %>% 
+  filter(LGDMarkAD == "AI") %>%
+  filter(!is.na(LGDNumPIT))
+
+# write valid tag list to .txt
+valid_tag_file_nm = paste0(tags_folder, "/LGR_", spc, "_SY", yr, ".txt")
+tag_list = valid_df %>%
+  select(LGDNumPIT) %>%
+  write.table(valid_tag_file_nm,
+              quote = F,
+              row.names = F,
+              col.names = F,
+              sep = "\t")
+
+### END SCRIPT
