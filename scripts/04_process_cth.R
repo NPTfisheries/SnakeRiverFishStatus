eaee2630d326a50e129512a681f50c83b8cd5cbf
Mike Ackerman <dirtpathwanderer@gmail.com>
2023-06-27 14:52:12 -0600

    initiating script to process CTHs using PITcleanr

---
+++ scripts/04_process_cth.R
@@ -0,0 +1,10 @@
+# -----------------------
+# Author(s): Ryan N. Kinzer, and Mike Ackerman
+# Purpose: Process complete tag histories for DABOM using PITcleanr
+# 
+# Created Date: June 28, 2021
+#   Last Modified: June 27, 2023
+#
+# Notes: 
+
+# start tomorrow by querying CTHs for all valid tag lists
