b3539b5825d80e61b69dc06a8fd081f7cf53cc25
Mike Ackerman <dirtpathwanderer@gmail.com>
2023-08-11 10:42:41 -0600

    draft 05_dabom.R to execute a DABOM model run

--- scripts/05_dabom.R
+++ scripts/05_dabom.R
@@ -3,7 +3,7 @@
 # Purpose: Run the DABOM model
 # 
 # Created Date: Unknown
-#   Last Modified: August 7, 2023
+#   Last Modified: August 10, 2023
 #
 # Notes: 
 
@@ -11,20 +11,23 @@
 library(tidyverse)
 library(here)
 library(PITcleanr)
+#library(sf)
 
 # install DABOM, if necessary
 # remotes::install_github("KevinSee/DABOM", build_vignettes = T)
-remotes::install_github("mackerman44/DABOM", head = "master")
+# remotes::install_github("mackerman44/DABOM", head = "npt_develop")
 library(DABOM)
 
 #--------------------
 # some initial setup
 # load configuration
 load(here("data/configuration_files/site_config_GRA.rda"))
+# configuration = configuration %>%
+#   st_drop_geometry()
 node_order = buildNodeOrder(pc_nodes) # build all of the paths to each detection location based on parent-child relationships
 
 # load trap_df to get origin
-trap_df = read_csv(here("data/LGTrappingDB/LGTrappingDB_2023-06-27.csv"))
+trap_df = read_csv(here("data/LGTrappingDB/LGTrappingDB_2023-08-10.csv"))
 
 # set folder for DABOM results
 dabom_folder = here("output/dabom_results/")
@@ -73,7 +76,7 @@ origin_df %>%
 # any tags have both a H and W record?
 duplicates = origin_df$tag_code[duplicated(origin_df$tag_code)] # eventually consider doing something about these
 
-# DABOM is capable of fitting a model w/ both H and W; filter is inc_hatchery = FALSE
+# DABOM is capable of fitting a model w/ both H and W; filter if inc_hatchery = FALSE
 if(inc_hatchery == FALSE) {
   origin_df = filter(origin_df, origin == "W")
   filter_ch = filter_ch %>%
@@ -103,25 +106,90 @@ bad_tags = bad_paths %>%
 
 # write default, initial jags model
 init_mod_file = here("model_files/lgr_dabom_jags.txt")
-writeDABOM(file_name = init_mod_file,
-           parent_child = parent_child,
-           configuration = configuration,
-           time_varying = TRUE)
-
-source(here("R/writeDABOM_KS.R"))
-source(here("R/getNodeInfo_RK.R"))
-source(here("R/addParentChildNodes_RK.R"))
-source(here("R/defineDabomColNms_RK.R"))
+writeDABOM_LGR(file_name = init_mod_file,
+               parent_child = parent_child,
+               configuration = configuration,
+               time_varying = TRUE)
 
 # write species and year specific jags model
 final_mod_file = here(paste0("model_files/lgr_dabom_jags_", spc, "_SY", yr, ".txt"))
-fixNoFishNodes(init_file = init_mod_file,
-               file_name = final_mod_file,
-               filter_ch = filter_ch,
-               parent_child = parent_child,
-               configuration = configuration,
-               fish_origin = origin_df)
+fixNoFishNodes_LGR(init_file = init_mod_file,
+                   file_name = final_mod_file,
+                   filter_ch = filter_ch,
+                   parent_child = parent_child,
+                   configuration = configuration,
+                   fish_origin = origin_df)
 
+# create a function to spit out initial values for MCMC chains
+init_fnc = setInitialValues_LGR(filter_ch = filter_ch,
+                                parent_child = parent_child,
+                                configuration = configuration)
 
+# create all the input data for the JAGS model
+jags_data = createJAGSinputs_LGR(filter_ch = filter_ch,
+                                 parent_child = parent_child,
+                                 configuration = configuration,
+                                 fish_origin = origin_df)
 
+# add data for time-varying models
+time_varying = T
+if(time_varying) {
+  if(spc == "Steelhead") {
+    jags_data = c(jags_data,
+                  addTimeVaryData(filter_ch = filter_ch,
+                                  start_date = paste0(yr-1,'0701'), 
+                                  end_date = paste0(yr,'0630')))
+  }
+  if(spc == "Chinook") {
+    jags_data = c(jags_data,
+                  addTimeVaryData(filter_ch = filter_ch,
+                                  start_date = paste0(yr,'0301'), 
+                                  end_date = paste0(yr,'0817')))
+  }
+} # end if time_varying
 
+# tell JAGS which parameters in the model that it should save
+test_mod_file = here(paste0("model_files/lgr_dabom_jags_", spc, "_SY", yr, "_test.txt"))
+jags_params = setSavedParams(model_file = final_mod_file)
+
+# set mcmc parameters (full run)
+# n.chains = 4
+# n.adapt  = 100
+# n.burn   = 1000
+# n.iter   = 5000
+# n.thin   = 10
+
+# set mcmc parameters (test run)
+n.chains = 4
+n.adapt  = 100
+n.burn   = 10
+n.iter   = 100
+n.thin   = 10
+
+# run on a single core for testing
+library(rjags)
+dabom_output = jags.model(file = test_mod_file,
+                          data = jags_data,
+                          inits = init_fnc,
+                          n.chains = n.chains,
+                          n.adapt = n.adapt)
+
+# or run using parallel cores
+source(here("R/run_dabom_parallel_v2.R"))
+dabom_output = run_dabom_parallel_v2(model = final_mod_file,
+                                     data = jags_data,
+                                     jags_params = jags_params,
+                                     inits = init_fnc,
+                                     n.chains = n.chains,
+                                     n.adapt = n.adapt,
+                                     n.burn = n.burn,
+                                     n.iter = n.iter,
+                                     thin = n.thin,
+                                     filter_ch = filter_ch,
+                                     filename = paste0(dabom_folder, "/lgr_dabom", spc, "_SY", yr, ".rda"))
+
+# save results to dabom_folder
+save(dabom_output,
+     filter_ch,
+     parent_child,
+     file = paste0(dabom_folder, "/lgr_dabom", spc, "_SY", yr, ".rda"))
