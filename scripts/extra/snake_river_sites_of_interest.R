cb40e0afe6d6af2d590fc51a40f6ebc196d41965
Mike Ackerman <dirtpathwanderer@gmail.com>
2023-10-10 15:36:19 -0600

    draft sites of interest

--- scripts/extra/snake_river_sites_of_interest.R
+++ scripts/extra/snake_river_sites_of_interest.R
@@ -22,9 +22,7 @@ library(tidyverse)
 library(here)
 library(sf)
 
-#----------------------
-# INT Sites
-# get polygon to define area of interest; only need steelhead polygon
+# get Snake River steelhead DPS polygon to filter area of interest
 load(here("data/spatial/SR_pops.rda")) ; rm(fall_pop, spsm_pop)
 sr_sthd_pops = st_as_sf(sth_pop) %>%
   select(sthd_DPS = ESU_DPS, 
@@ -37,16 +35,14 @@ sr_sthd_pops = st_as_sf(sth_pop) %>%
 sthd_pop_p = sr_sthd_pops %>%
   ggplot() +
   geom_sf(aes(fill = sthd_MPG)) +
-  theme_bw()
+  theme_bw() +
+  labs(title = "Snake River Steelhead DPS",
+       fill = "MPG") +
+  theme(legend.position = "bottom")
 sthd_pop_p
 
 # query metadata for all INT and MRR sites in PTAGIS
 ptagis_sf = buildConfig(node_assign = "site") %>%
-  # make spatial
-  filter(!is.na(latitude) | !is.na(longitude)) %>%
-  st_as_sf(coords = c("longitude", 
-                      "latitude"), 
-           crs = 4326) %>%
   # clean things up a bit
   select(site_code,
          site_name,
@@ -54,56 +50,71 @@ ptagis_sf = buildConfig(node_assign = "site") %>%
          site_type_name,
          start_date,
          end_date,
-         config_id,
          rkm,
          rkm_total,
-         geometry)
+         latitude,
+         longitude,
+         site_description) %>%
+  # and make spatial
+  filter(!is.na(latitude) | !is.na(longitude)) %>%
+  st_as_sf(coords = c("longitude", 
+                      "latitude"), 
+           crs = 4326) %>%
+  # trim down to unique sites
+  distinct(site_code,
+           site_name,
+           site_type,
+           rkm,
+           geometry,
+           .keep_all = T)
 
 # trim down to sites within the Snake River steelhead DPS
-sr_sites = ptagis_sf %>%
+snake_river_sites = ptagis_sf %>%
   st_join(sr_sthd_pops) %>%
-  filter(!is.na(sthd_DPS)) %>%
-  # trim down to unique sites
-  group_by(site_code, geometry) %>%
-  slice(1)
+  filter(!is.na(sthd_DPS)) 
 
+#----------------------
 # Snake River INT sites
-sr_int_sites = sr_sites %>%
+sr_int_sites = snake_river_sites %>%
+  # grab only INT sites for now 
   filter(site_type == "INT") %>%
-  # remove some Snake River dams within steelhead DPS. These will get added back in later.
+  # remove some dam sites; we'll deal with those later
   filter(!str_detect(site_name, "Lower Granite")) %>%
   filter(!str_detect(site_name, "LOWER GRANITE")) %>%
   filter(!str_detect(site_name, "Little Goose")) %>%
   filter(!str_detect(site_name, "Lower Monumental")) %>%
-  # remove additional unnecessary INT sites
-  filter(!site_code %in% c("CCP",  # Catherine Creek Acclimation Pond
-                           "CHN",  # Challis Diversion North
-                           "CHS",  # Challis Diversion South
-                           "CLJ",  # Clearwater River Juvenile Fish Trap
-                           "GRP",  # Grande Ronde Acclimation Pond
-                           "IMJ",  # Imnaha River Juvenile Fish Trap
-                           "LGW",  # Lookingglass Creek Weir
-                           "LOP",  # Lostine River Acclimation Pond
-                           "PWA",  # Penawawa Creek
-                           "RPJ",  # Rapid River Hatchery Pond
-                           "S2I",  # Lemhi Sub-reach 2 SC Inlet
-                           "S2O",  # Lemhi Sub-reach 2 SC Outlet
-                           "S3A",  # Eagle Valley Ranch S3A
-                           "S3B",  # Eagle Valley Ranch S3B
-                           "SAJ",  # Salmon River Trap
-                           "SNJ")) # Snake River Trap
+  # remove unnecessary INT sites we don't want
+  filter(!site_code %in% c("CCP", # Catherine Creek Acclimation Pond
+                           "GRP", # Grande Ronde Acclimation Pond
+                           "LOP", # Lostine River Acclimation Pond
+                           "RPJ", # Rapid River Hatchery Pond
+                           "S2I", # Lemhi Sub-reach 2 SC Inlet
+                           "S2O", # Lemhi Sub-reach 2 SC Outlet
+                           "S3A", # Eagle Valley Ranch S3A
+                           "S3B", # Eagle Valley Ranch S3B
+                           "CHN", # Challis Diversion North
+                           "CHS", # Challis Diversion South
+                           "CLJ", # Clearwater River Juvenile Fish Trap
+                           "IMJ", # Imnaha River Juvenile Fish Trap
+                           "SAJ", # Salmon River Trap
+                           "SNJ", # Snake River Trap
+                           "LGW")) # Lookingglass Creek weir (not sure why this wasn't in previous models)
+
+# "PWA" (Penawawa Creek) wasn't included in previous DABOM model runs; adding in for now
 
 #----------------------
-# MRR Sites
-# read in all complete tag histories since SY2010
+# Snake River MRR Sites
+# read in complete tag histories since SY2010
 cth_df = list.files(path = here("data/complete_tag_histories/"),
                     pattern = "\\.csv$",
                     full.names = T) %>%
   setNames(nm = .) %>%
   map_df(~read_csv(.x), .id = "file_name") %>%
-  mutate(file_name = str_replace(file_name, ".*/", "")) %>%    # extract file name
-  mutate(species = str_extract(file_name, "(?<=_)[^_]+")) %>%  # extract species
-  mutate(spawn_year = str_extract(file_name, "SY[0-9]{4}"))    # extract spawn year
+  # add species and spawn year
+  mutate(file_name = str_replace(file_name, ".*/", ""), 
+         species = str_extract(file_name, "(?<=_)[^_]+"),       
+         spawn_year = str_extract(file_name, "SY[0-9]{4}")) %>%
+  select(-file_name)
 
 # summarize number of tags observed by site
 tags_by_site = cth_df %>%
@@ -120,74 +131,70 @@ tags_by_site = cth_df %>%
     names_from = c(species, spawn_year),
     values_from = n_tags) %>%
   mutate(n_chnk_tags = rowSums(select(., starts_with("Chinook")), na.rm = TRUE),
-         n_sthd_tags = rowSums(select(., starts_with("Steelhead")), na.rm = TRUE)) %>%
-  select(site_code,
-         n_chnk_tags,
-         n_sthd_tags)
+         n_sthd_tags = rowSums(select(., starts_with("Steelhead")), na.rm = TRUE))
 
 # join detections to MRR sites
-sr_mrr_sites = sr_sites %>%
-  # filter for MRR sites that have at least 50 tags observed for Chinook salmon or steelhead
+sr_mrr_sites = snake_river_sites %>%
+  # grab only MRR sites
   filter(site_type == "MRR") %>%
-  left_join(tags_by_site) %>%
+  left_join(tags_by_site %>%
+              select(site_code,
+                     n_chnk_tags,
+                     n_sthd_tags)) %>%
+  # filter for only sites where there have been 50 or greater detections for Chinook salmon or steelhead
   filter(!n_chnk_tags <= 50 | !n_sthd_tags <= 50) %>%
-  # remove Lower Granite MRR sites for now
+  # remove Lower Granite Dam MRR sites
   filter(!str_detect(site_code, "LGR")) %>%
+  # remove acclimation ponds 
+  filter(!str_detect(site_description, "AcclimationPond")) %>%
+  # Do I want to leave out all hatcheries? These detections seem unreliable i.e., what's their disposition?
+  filter(!str_detect(site_description, "Hatchery")) %>%
   select(-n_chnk_tags,
          -n_sthd_tags) %>%
-  # add back in some mrr sites that were included in previous DABOM model runs
-  bind_rows(sr_sites %>%
-              filter(site_code %in% c("ALMOTC",
-                                      "BIGBEC",
-                                      "CAMP4C",
-                                      "DRY2C",
-                                      "EFPW",
-                                      "FREEZC",
-                                      "GUMBTC",
-                                      "KOOS",
-                                      "LBEARC",
-                                      "MAHOGC",
-                                      "PENAWC",
-                                      "POTRWF")))
-# NEED TO FINISH THE MRR SECTION!!!
+  # finally, add back in some MRR sites that have been included in previous DABOM model runs
+  bind_rows(snake_river_sites %>%
+              filter(site_code %in% c("ALMOTC",   # Almota Creek
+                                      "BIGBEC",   # Big Bear Creek
+                                      "CAMP4C",   # Camp Creek
+                                      "DRY2C",    # Dry Creek
+                                      "EFPW",     # EF Potlatch Weir
+                                      "FREEZC",   # Freezout Creek
+                                      "GUMBTC",   # Gumboot Creek
+                                      # "KOOS",   # Kooskia National Fish Hatchery
+                                      "LBEARC",   # Little Bear Creek
+                                      "MAHOGC",   # Mahogany Creek
+                                      "PENAWC",   # Penawawa Creek
+                                      "POTRWF"))) # West Fork Potlatch River
 
 #----------------------
 # Dam Sites
 dam_sites = ptagis_sf %>%
-  mutate(
-    site_code = case_when(
-      site_code %in% c("GRA", "LGRLDR", "LGR")            ~ "GRA", # LGR Adults
-      site_code %in% c("GRJ", "GRX", "GRS")               ~ "GRS", # LGR Juveniles; GRJ, GRX = LGR Juvenile Bypass; GRS = LGR Spillway
-      site_code %in% c("LGS", "GOJ", "GOA")               ~ "LGS", # Little Goose Dam
-      site_code %in% c("LMN", "LMJ", "LMA")               ~ "LMN", # Lower Monumental Dam
-      site_code %in% c("IHR", "ICH", "IHA")               ~ "IHR", # Ice Harbor Dam
-      site_code %in% c("MCN", "MCJ", "MCX", "MC1", "MC2") ~ "MCN", # McNary Dam
-      site_code %in% c("JDJ", "JDA", "JO1", "JO2")        ~ "JDA", # John Day Dam
-      site_code %in% c("TDA", "TD1", "TD2")               ~ "TDA", # The Dalles Dam
-      site_code %in% c("B2A", "BONAFF", "BO1", "BO2", 
-                       "BON", "BVJ", "B1J", "BVX", "B2J",
-                       "BO4", "BWL", "BO3", "BCC")        ~ "BON", # Bonneville Dam
-      TRUE ~ site_code)) %>%
-  filter(site_code %in% c("GRA", "GRS", "LGS", "LMN", "IHR", "MCN", "JDA", "TDA", "BON"))
+  filter(site_code %in% c("GRA", "LGRLDR", "LGR",            # LGR Adults
+                          "GRJ", "GRS", "GRX",               # LGR Juveniles; GRJ, GRX = LGR Juvenile Bypass; GRS = LGR Spillway
+                          "LGS", "GOJ", "GOA",               # Little Goose Dam
+                          "LMA", "LMN", "LMJ",               # Lower Monumental Dam
+                          "IHA", "IHR", "ICH",               # Ice Harbor Dam
+                          "MCN", "MCJ", "MCX", "MC1", "MC2", # McNary Dam
+                          "JDA", "JDJ", "JO1", "JO2",        # John Day Dam
+                          "TDA", "TD1", "TD2",               # The Dalles Dam
+                          "B1J", "B2J", "B2A", "BVJ", "BVX", # Bonneville Dam
+                          "BO1", "BO2", "BO3", "BO4",
+                          "BON", "BCC", "BWL", "BONAFF"))
 
 #----------------------
-# Downriver Subbasins
+# Downriver Sub-basin Sites; here we're just grabbing the lowest-most site from each of the downstream sub-basins of interest
 downriver_sites = ptagis_sf %>%
-  mutate(
-    site_code = case_when(
-      as.numeric(str_extract(rkm, "\\d+")) > 539                                                             ~ "PRA", # Upper Columbia
-      as.numeric(str_extract(rkm, "\\d+")) == 539 & as.numeric(sub(".","", str_extract(rkm, "\\.\\d+"))) > 0 ~ "PRO", # Yakima 
-      as.numeric(str_extract(rkm, "\\d+")) == 509 & as.numeric(sub(".","", str_extract(rkm, "\\.\\d+"))) > 0 ~ "WWB", # Walla Walla 
-      as.numeric(str_extract(rkm, "\\d+")) == 465 & as.numeric(sub(".","", str_extract(rkm, "\\.\\d+"))) > 0 ~ "UMW", # Umatilla 
-      as.numeric(str_extract(rkm, "\\d+")) == 351 & as.numeric(sub(".","", str_extract(rkm, "\\.\\d+"))) > 0 ~ "JD1", # John Day 
-      as.numeric(str_extract(rkm, "\\d+")) == 328 & as.numeric(sub(".","", str_extract(rkm, "\\.\\d+"))) > 0 ~ "DRM", # Deschutes
-      as.numeric(str_extract(rkm, "\\d+")) == 290 & as.numeric(sub(".","", str_extract(rkm, "\\.\\d+"))) > 0 ~ "KLR", # Klickitat 
-      as.numeric(str_extract(rkm, "\\d+")) == 273 & as.numeric(sub(".","", str_extract(rkm, "\\.\\d+"))) > 0 ~ "FID", # Hood River
-      as.numeric(str_extract(rkm, "\\d+")) == 271 & as.numeric(sub(".","", str_extract(rkm, "\\.\\d+"))) > 0 ~ "RCX", # White Salmon
-      as.numeric(str_extract(rkm, "\\d+")) == 261 & as.numeric(sub(".","", str_extract(rkm, "\\.\\d+"))) > 0 ~ "LWL", # Little White 
-      as.numeric(str_extract(rkm, "\\d+")) == 251 & as.numeric(sub(".","", str_extract(rkm, "\\.\\d+"))) > 0 ~ "WRA", # Wind River
-      TRUE ~ site_code)) %>%
-  filter(site_code %in% c("PRA", "PRO", "WWB", "UMW", "JD1", "DRM", "KLR", "FID", "RCX", "LWL", "WRA"))
+  filter(site_code %in% c("PRA",  # Priest Rapids Dam (Upper Columbia)
+                          "PRO",  # Prosser Dam (Yakima)
+                          "WWB",  # Walla Walla River Barge IPTDS
+                          "UMW",  # Umatilla River Recycled Water Facility IPTDS
+                          "JD1",  # John Day River, McDonald Ferry IPTDS
+                          "DRM",  # Deschutes River Mouth IPTDS
+                          "KLR",  # Klickitat River Floating IPTDS
+                          "FID",  # Farmer's Diversion IPTDS (Hood River)
+                          "RCX",  # Rattlesnake Creek IPTDS (White Salmon)
+                          "LWL",  # Little White Salmon NFH (Little White Salmon)
+                          "WRA")) # Upper Wind River IPTDS   
 
 #----------------------
 # bind them all together
@@ -201,4 +208,3 @@ write_csv(sites_of_interest,
           here("data/configuration_files/sites_of_interest.csv"))
 
 # END SCRIPT
-
