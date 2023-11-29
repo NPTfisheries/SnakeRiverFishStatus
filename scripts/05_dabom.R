6f00da0b29b9cce8dbd7ea88f11ae6ee2b147c8d
Mike Ackerman <dirtpathwanderer@gmail.com>
2023-11-29 07:08:24 -0700

    saved SY2012 Chinook DABOM results

--- scripts/05_dabom.R
+++ scripts/05_dabom.R
@@ -33,7 +33,7 @@ if(!dir.exists(dabom_folder)) { dir.create(dabom_folder) }
 # start analysis
 # set species and spawn year
 spc = "Chinook"
-yr = 2010
+yr = 2012
 
 if(spc == "Chinook")   { spc_code = 1 }
 if(spc == "Steelhead") { spc_code = 3 }
