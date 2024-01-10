08beb1859c7645e87a082deb24e60d4903661e87
Mike Ackerman <dirtpathwanderer@gmail.com>
2024-01-10 16:28:15 -0700

    draft of 08_life_history_model_run.R

--- scripts/08_life_history_model_run.R
+++ scripts/08_life_history_model_run.R
@@ -27,6 +27,7 @@ lh_folder = "output/life_history/"
 spc = "Chinook"
 yr = 2010
 
+#-----------------
 # create JAGS model to estimate female proportion
 sex_model_nm = here("model_files/female_prop_jags.txt")
 
@@ -83,9 +84,173 @@ sex_mod = jags(data = sex_jags_data,
                n.thin = 20,
                verbose = F)
 
+# save results
 save(sex_mod,
      sex_jags_data,
      mod_sex_df,
-     file = paste0(here)
+     file = paste0(here(), "/", sex_folder, "SY", yr, "_", spc, "_pop_sex_prop.rda"))
 
-paste0(here(), sex_folder)
+#-----------------
+# create JAGS model to estimate age proportions
+
+# simple model to be used for Chinook
+age_mod_simp_nm = here("model_files/age_prop_simp_jags.txt")
+
+model_code = "
+data {
+  D <- dim(age_mat)
+}
+
+model {
+
+  for(i in 1:D[1]) {
+    age_mat[i,] ~ dmulti(pi[pop_num[i],], tags[i])
+  }
+
+  for(k in 1:D[2]) {
+    alpha[k] <- 1
+  }
+
+  for(j in 1:max(pop_num)) {
+    pi[j,1:D[2]] ~ ddirch(alpha[1:D[2]])
+  }
+
+ }"
+
+cat(model_code, file = age_mod_simp_nm)
+
+# hierarchical model to be used for steelhead
+age_mod_hier_nm = here("model_files/age_prop_hier_jags.txt")
+
+model_code = "
+data {
+  D <- dim(age_mat)
+}
+
+model {
+
+  for(i in 1:D[1]) {
+    age_mat[i,] ~ dmulti(pi[pop_num[i],], tags[i])
+  }
+  
+  # multivariate logistic normal transformation to make it hierarchical
+  for(j in 1:max(pop_num)) {
+    p[j, 1] <- 0
+    p[j,2:D[2]] ~ dmnorm(mu[run_type[j], 1:(D[2] - 1)], Tau[1:(D[2] - 1), 1:(D[2] - 1)])
+    
+    sum_exp_p[j] <- sum(exp_p[j,])
+    
+    for(k in 1:D[2]) {
+      exp_p[j,k] = exp(p[j, k])
+      pi[j, k] <- exp(p[j, k]) / sum_exp_p[j]
+    }
+  }
+  
+  # transform mu back to proportions
+  for(j in 1:max(runType)) {
+    muProp[j,1] = 0
+    for(i in 2:D[2]) {
+      muProp[j,i] = mu[j,i-1]
+    }
+    sum_exp_mu[j] = sum(exp_mu[j,])
+    for(i in 1:D[2]) {
+      exp_mu[j,i] = exp(muProp[j,i])
+      avgPi[j,i] = exp_mu[j,i] / sum_exp_mu[j]
+    }
+  }
+  
+  # Cauchy prior on the MVN mean vector
+  for(i in 1:(D[2] - 1)) {
+    for(j in 1:max(run_type)) {
+      mu[j, i] ~ dt(0, 0.001, 1)
+    }
+  }
+  # Priors on the precision matrix
+  Tau ~ dwish(R, k)
+  k <- D[2] + 1
+
+}"
+
+cat(model_code, file = age_mod_hier_nm)
+
+#-----------------
+# run age model
+
+if(spc == "Chinook") { model = "simple" }
+if(spc == "Steelhead") { model = "hierarchical" }
+
+if(model == "simple") {
+  age_model_nm = age_mod_simp_nm
+  jags_param = "pi"
+} else {
+  age_model_nm = age_mod_hier_nm
+  jags_param = c("pi", "mu", "Tau", "avgPi")
+}
+
+# read in data
+mod_age_df = read_excel(paste0(here(), "/", lh_folder, spc, "_SY", yr, "_lh_summary.xlsx"),
+                        "age_df",
+                        progress = F)
+
+# pull out relevant bits for JAGS, and name them appropriately
+age_jags_data = mod_age_df %>%
+  filter(TRT_POPID != "Not Observed") %>%
+  filter(species != "Total") %>%
+  mutate(pop_num = as.integer(as.factor(TRT_POPID))) %>%
+  select(tags = n_aged,
+         pop_num) %>%
+  as.list()
+
+age_jags_data$age_mat = mod_age_df %>%
+  filter(TRT_POPID != "Not Observed") %>%
+  filter(species != "Total") %>%
+  select(starts_with("age")) %>%
+  as.matrix()
+  
+# drop ages with no observed fish in them
+if(sum(colSums(age_jags_data$age_mat) == 0) > 0) {
+  age_jags_data$age_mat = age_jags_data$age_mat[,!colSums(age_jags_data$age_mat) == 0]
+}
+
+# gather steelhead data specific to hierarchical model
+if(model == "hierarchical"){
+  
+  age_jags_data$run_type = mod_age_df %>%
+    filter(TRT_POPID != "Not Observed") %>%
+    filter(species != "Total") %>%
+    select(TRT_POPID) %>%
+    mutate(run = if_else(TRT_POPID %in% c('CRLMA-s',
+                                          'CRLOC-s',
+                                          'CRLOL-s',
+                                          'CRSEL-s',
+                                          'CRSFC-s',
+                                          'MFBIG-s',
+                                          'SFMAI-s',
+                                          'SFSEC-s'),
+                         "B", "A")) %>%
+    distinct() %>%
+    mutate(run = as.factor(run),
+           run_type = as.integer(run)) %>%
+    pull(run_type)
+  
+  age_jags_data$r = diag(1, ncol(age_jags_data$age_mat) - 1)
+  
+}
+
+# run JAGS model
+age_mod = jags(data = age_jags_data,
+               parameters.to.save = jags_params,
+               model.file = age_model_nm,
+               n.chains = 3,
+               n.iter = 20000,
+               n.burnin = 10000,
+               n.thin = 20,
+               verbose = T)
+
+# save results
+save(age_mod,
+     age_jags_data,
+     mod_age_df,
+     file = paste0(here(), "/", age_folder, "SY", yr, "_", spc, "_pop_age_prop.rda"))
+
+# END SCRIPT
