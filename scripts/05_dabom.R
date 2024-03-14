bd82b535a016592767b9074dafe6579231811676
Mike Ackerman <dirtpathwanderer@gmail.com>
2024-03-14 09:55:28 -0600

    adding sy14, 15, 18 steelhead lh results

--- scripts/05_dabom.R
+++ scripts/05_dabom.R
@@ -33,7 +33,7 @@ if(!dir.exists(dabom_folder)) { dir.create(dabom_folder) }
 # start analysis
 # set species and spawn year
 spc = "Steelhead"
-yr = 2014
+yr = 2018
 
 if(spc == "Chinook")   { spc_code = 1 }
 if(spc == "Steelhead") { spc_code = 3 }
