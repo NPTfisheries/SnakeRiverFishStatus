db2469d334d6ccc299ab2fac480f0da994a8595a
Mike Ackerman <dirtpathwanderer@gmail.com>
2024-06-10 15:09:41 -0600

    coho stadem results sy21-23

--- scripts/01_stadem_run.R
+++ scripts/01_stadem_run.R
@@ -11,7 +11,7 @@
 rm(list = ls())
 
 # install STADEM from GitHub, if not already available
-remotes::install_github("mackerman44/STADEM", ref = "npt_coho")
+#remotes::install_github("mackerman44/STADEM", ref = "npt_coho")
 #remotes::install_github("KevinSee/STADEM", ref = "main")
 
 # load packages
