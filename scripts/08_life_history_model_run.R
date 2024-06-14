06c7d87cfdb39145ad611cb88cf41c5426d9cd8b
Mike Ackerman <dirtpathwanderer@gmail.com>
2024-06-14 10:32:40 -0600

    just saving latest 08 script

--- scripts/08_life_history_model_run.R
+++ scripts/08_life_history_model_run.R
@@ -17,8 +17,8 @@ library(readxl)
 library(jagsUI)
 
 # set species and year
-spc = "Chinook"
-yr = 2014
+spc = "Steelhead"
+yr = 2023
 
 # set up folder structure for output
 sex_folder = "output/sex_results/"
