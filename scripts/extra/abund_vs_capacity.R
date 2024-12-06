570a74cff3c6e678e53b4d715539701b1ed3262f
Mike Ackerman <dirtpathwanderer@gmail.com>
2024-12-06 11:05:40 -0700

    whoops, forgot to save script

--- scripts/extra/abund_vs_capacity.R
+++ scripts/extra/abund_vs_capacity.R
@@ -15,7 +15,17 @@ rm(list = ls())
 library(tidyverse)
 library(here)
 library(sf)
+library(readxl)
 
 #-------------------
 # load and prep data
 
+# read in population abundance estimates
+pop_abund_df = read_xlsx(path = here("output/syntheses/LGR_Chinook_all_summaries_2024-12-05.xlsx"),
+                         sheet = "Pop_Tot_Esc",
+                         col_types = c(rep("text", 6),
+                                       rep("numeric", 13))) %>%
+  bind_rows(read_xlsx(path = here("output/syntheses/LGR_Steelhead_all_summaries_2024-12-05.xlsx"),
+                      sheet = "Pop_Tot_Esc",
+                      col_types = c(rep("text", 6),
+                                    rep("numeric", 13))))
