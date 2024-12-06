338185d328af6a72af20da5e33d267be8f6f3bb7
Mike Ackerman <dirtpathwanderer@gmail.com>
2024-12-06 11:05:12 -0700

    started a script to compare abundances vs. capacity

---
+++ scripts/extra/abund_vs_capacity.R
@@ -0,0 +1,21 @@
+# -----------------------
+# Author(s): Mike Ackerman and Ryan N. Kinzer
+# Purpose: Compare trt population abundance estimates vs. estimates of available spawning
+#            habitat capacity i.e., how saturated are populations?
+# 
+# Created Date: December 6, 2024
+#   Last Modified: 
+#
+# Notes: 
+
+# clear environment
+rm(list = ls())
+
+# load necessary libraries
+library(tidyverse)
+library(here)
+library(sf)
+
+#-------------------
+# load and prep data
+
