2fb9a7213a707ab31058efe0141c628200da2218
Mike Ackerman <dirtpathwanderer@gmail.com>
2025-02-12 16:02:47 -0700

    moved lgr_kelt_eval.R from SBSA

---
+++ scripts/extra/lgr_kelt_eval.R
@@ -0,0 +1,138 @@
+# -----------------------
+# Author(s): Mike Ackerman
+# Purpose: Evaluate kelting rates at Lower Granite Dam
+# 
+# Created Date: February 12, 2025
+#   Last Modified: 
+#
+# Notes:
+
+# clear environment
+rm(list = ls())
+
+# load necessary libraries
+library(tidyverse)
+library(here)
+library(readxl)
+library(PITcleanr)
+
+#---------------
+# lgr valid tags
+
+# valid tag lists
+# valid_tag_df = list.files(path = here("output/valid_tag_lists/"),
+#                           pattern = "Steelhead.*\\.txt$",
+#                           full.names = T) %>%
+#   set_names() %>%
+#   map_df(
+#     ~ read_csv(.x,
+#                col_names = "tag_code",
+#                show_col_types = FALSE) %>%
+#       mutate(spawn_yr = str_extract(.x, "SY\\d{4}") %>%
+#                str_remove("SY") %>%
+#                as.numeric())
+#   ) %>%
+#   group_by(spawn_yr) %>%
+#   summarize(n_tags = n_distinct(tag_code))
+
+# PITcleanr cleaned steelhead observation data
+pitcleanr_df = list.files(path = here("output/PITcleanr/human_reviewed/"),
+                          pattern = "Steelhead",
+                          full.names = TRUE) %>%
+  map_df(~ {
+    # extract the spawn year from the file name
+    spawn_yr <- str_extract(.x, "(?<=SY)\\d{4}")
+    read_xlsx(path = .x, sheet = "Sheet1") %>%
+      mutate(spawn_yr = as.numeric(spawn_yr))
+  })
+
+# valid tags per spawn year, pitcleanr data
+valid_tag_df = pitcleanr_df %>%
+  group_by(spawn_yr) %>%
+  summarize(n_tags = n_distinct(tag_code))
+
+#---------------
+# lgr natural origin escapement
+lgr_esc_df = read_xlsx(path = here("output/syntheses/LGR_Steelhead_all_summaries_2025-01-31.xlsx"),
+                       sheet = "LGR_Esc") %>%
+  filter(origin == "Natural") %>%
+  select(-mean, -sd)
+
+#---------------
+# kelt configuration file
+load(here("data/configuration_files/site_config_LGR_20241226.rda")) ; rm(flowlines, parent_child, configuration, sr_site_pops)
+
+#---------------
+# kelt complete tag histories
+kelt_cth_df = pitcleanr_df %>%
+  # trim to kelts
+  group_by(spawn_yr, tag_code) %>%
+  filter(any(life_stage == "kelt")) %>%
+  ungroup() %>%
+  # create site from node
+  mutate(site_code = str_remove(node, "_[D|U]")) %>%
+  select(spawn_yr,
+         id,
+         tag_code,
+         life_stage,
+         node,
+         site_code,
+         direction,
+         slot,
+         min_det,
+         tag_start_date,
+         node_order,
+         path) %>%
+  # remove repeat spawner observations, for now
+  filter(life_stage != "repeat spawner")
+
+# kelt simple capture histories
+kelt_ch_df = kelt_cth_df %>%
+  # join rkm
+  left_join(crb_sites_sf %>% select(site_code, rkm, rkm_total), by = "site_code") %>%
+  group_by(spawn_yr, tag_code) %>%
+  summarise(
+    ups = if_else(any(str_starts(rkm, "522") & rkm_total > 695), 1, 0),
+    grs = if_else(any(site_code == "GRS" & life_stage == "kelt"), 1, 0),
+    dwn = if_else(any(site_code %in% c("GOA", "LMA", "IHR", "MCN", "JDA", "TDA", "BON") & life_stage == "kelt"), 1, 0)
+  ) %>%
+  ungroup()
+
+# summarize capture histories
+kelt_tbl = kelt_ch_df %>%
+  group_by(spawn_yr) %>%
+  summarise(
+    n_tag_kelt_grs = sum(grs == 1),
+    n_tag_kelt_dwn = sum(dwn == 1),
+    n_tag_kelt_grs_dwn = sum(grs == 1 & dwn == 1)
+  ) %>%
+  ungroup() %>%
+  left_join(valid_tag_df) %>%
+  select(spawn_yr,
+         n_tags,
+         everything()) %>%
+  mutate(grs_kelt_det_prob = n_tag_kelt_grs_dwn / n_tag_kelt_dwn,
+         est_tag_kelt_lgr = n_tag_kelt_grs / grs_kelt_det_prob,
+         kelt_rate = est_tag_kelt_lgr / n_tags)
+
+# kelt_tbl_w_cis = kelt_ch_df %>%
+#   group_by(spawn_yr) %>%
+#   summarise(
+#     n_tag_kelt_grs = sum(grs == 1),
+#     n_tag_kelt_dwn = sum(dwn == 1),
+#     n_tag_kelt_grs_dwn = sum(grs == 1 & dwn == 1)
+#   ) %>%
+#   ungroup() %>%
+#   left_join(valid_tag_df) %>%
+#   select(spawn_yr,
+#          n_tags,
+#          everything()) %>%
+#   # lgr kelt detection probability
+#   mutate(
+#     grs_kelt_det = binom.confint(n_tag_kelt_grs_dwn, n_tag_kelt_dwn, method = "wilson"),
+#     grs_kelt_det_mean = round(grs_kelt_det$mean, 3),
+#     grs_kelt_det_95ci = paste("(", round(grs_kelt_det$lower, 3), ", ", round(grs_kelt_det$upper, 3), ")", sep = "")
+#   ) %>%
+#   select(-grs_kelt_det)
+
+### END SCRIPT
