77553d298b08b78049a548590bb53321966e81fa
Mike Ackerman <dirtpathwanderer@gmail.com>
2025-04-21 10:28:33 -0600

    sy24 sthd lh model results

--- scripts/08_life_history_model_run.R
+++ scripts/08_life_history_model_run.R
@@ -3,7 +3,7 @@
 # Purpose: Run life history models to estimate sex ratio and age structure
 # 
 # Created Date: July 10, 2019
-#   Last Modified: April 18, 2025
+#   Last Modified: April 21, 2025
 #
 # Notes: 
 
@@ -17,7 +17,7 @@ library(readxl)
 library(jagsUI)
 
 # set species and year
-spc = "Chinook"
+spc = "Steelhead"
 yr = 2024
 
 # set up folder structure for output
