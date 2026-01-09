1ed3612569533ad0373bf7f53dd488ee38cf66c9
Mike Ackerman <dirtpathwanderer@gmail.com>
2026-01-09 13:40:57 -0700

    cleaned up 00 script

--- scripts/00_save_lgr_trapping_db.R
+++ scripts/00_save_lgr_trapping_db.R
@@ -1,41 +1,30 @@
 # -----------------------
 # Author(s): Ryan Kinzer and Mike Ackerman
 # Purpose: Open connection to the Lower Granite Dam trapping database (LGTrappingDB)
-#   and save master table as a .csv
+#   and save master table to a .csv
 # 
 # Created Date: Unknown
 #   Last Modified: January 6, 2026
 #
-# Notes:
+# Notes: due to file size, the Lower Granite trapping database is saved to a .gitignore directory i.e., the user needs to download their own local copy
 
 # clear environment
 rm(list = ls())
 
 # load packages
 library(tidyverse)
-library(here)
 
-# Requires a copy of the Lower Granite Dam trapping database and the ODBC driver
-if(.Platform$OS.type != "unix") { # includes windows machines
+# requires a copy of the Lower Granite Dam trapping database and the ODBC driver
+if(.Platform$OS.type != "unix") {                                       # includes windows machines
   
-  # set path to LGTrappingDB Access database
-  trap_filepath = here("data/LGTrappingDB/LGTrappingExport_2026-01-06.accdb")
-  
-  # source loadLGTrappingDBase function
-  source(here("R/loadLGTrappingDBase.R"))
-  
-  # connect to LGTrappingDB
-  con = loadLGTrappingDBase(trapDB_filepath = trap_filepath)
-  
-  # read master table within LGTrappingDB
-  trap_dbase = DBI::dbReadTable(con, 'tblLGDMasterCombineExport')
-  
-  # and disconnect
-  DBI::dbDisconnect(con)
-  
-} # end if not unix
+  trap_filepath = "data/LGTrappingDB/LGTrappingExport_2026-01-06.accdb" # set path to LGTrappingDB Access database
+  source("R/loadLGTrappingDBase.R")                                     # source loadLGTrappingDBase function
+  con = loadLGTrappingDBase(trapDB_filepath = trap_filepath)            # connect to database
+  trap_dbase = DBI::dbReadTable(con, 'tblLGDMasterCombineExport')       # read master table within LGTrappingDB
+  DBI::dbDisconnect(con)                                                # disconnect
+}
 
-# write .csv of LGTrappingDB for later use
+# write database to .csv
 write_csv(trap_dbase, file = paste0(here("data/LGTrappingDB/LGTrappingDB_/"), Sys.Date(), ".csv"))
 
 # END SCRIPT
