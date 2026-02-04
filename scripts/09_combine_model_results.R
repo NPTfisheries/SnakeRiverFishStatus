794f9d063a9cc34220b1aa6c9af34a4e8de7139e
Mike Ackerman <dirtpathwanderer@gmail.com>
2026-02-04 11:15:46 -0700

    sy10-14 chnk sfmai resolved

--- scripts/09_combine_model_results.R
+++ scripts/09_combine_model_results.R
@@ -22,8 +22,8 @@ library(magrittr)
 library(readxl)
 
 # set species and year
-spc = "Steelhead"
-yr = 2025
+spc = "Chinook"
+yr = 2010
 
 # load configuration files
 if (yr <  2024) { load("data/configuration_files/site_config_LGR_20240927.rda") }
